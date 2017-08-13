e166f41f86bee9076499422519d9224af54995f0
vksantoshsunny <vksantoshsunny@gmail.com>
2017-08-13 22:56:59 +1200

    Added EntityFramework and create DBContext

---
+++ Data/Configuration/EntityConfigure.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Configuration
+{
+	public class ProductConfigure : EntityTypeConfiguration<Product>
+	{
+		public ProductConfigure()
+		{
+			Property(x => x.Id)
+				.IsRequired();
+
+			Property(x => x.Name)
+				.IsRequired()
+				.HasMaxLength(50);
+
+			Property(x => x.Description)
+				.HasMaxLength(500);
+
+
+
+		}
+	}
+
+	public class ProductOptionConfigure : EntityTypeConfiguration<ProductOption>
+	{
+		public ProductOptionConfigure()
+		{
+			Property(x => x.Id)
+				.IsRequired();
+
+			Property(x => x.Name)
+				.IsRequired()
+				.HasMaxLength(50);
+
+			Property(x => x.Description)
+				.HasMaxLength(500);
+
+			HasRequired(x => x.Product)
+				.WithMany(y => y.ProductOptions)
+				.HasForeignKey(y => y.ProductId)
+				.WillCascadeOnDelete(true);
+		}
+	}
+
+}
