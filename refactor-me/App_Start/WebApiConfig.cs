5f4e50510a8a318fe1b45d70639b7faf032fddfe
vksantoshsunny <vksantoshsunny@gmail.com>
2017-08-13 23:30:14 +1200

    Updated the ProductsController and also resolved dependencies

--- refactor-me/App_Start/WebApiConfig.cs
+++ refactor-me/App_Start/WebApiConfig.cs
@@ -1,24 +1,35 @@
-﻿using System.Web.Http;
+﻿using AutoMapper;
+using Microsoft.Practices.Unity;
+using Repository;
+using Repository.Interfaces;
+using System.Web.Http;
+using FluentValidation.WebApi;
 
 namespace refactor_me
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
-            var formatters = GlobalConfiguration.Configuration.Formatters;
-            formatters.Remove(formatters.XmlFormatter);
-            formatters.JsonFormatter.Indent = true;
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+			var formatters = GlobalConfiguration.Configuration.Formatters;
+			formatters.Remove(formatters.XmlFormatter);
+			formatters.JsonFormatter.Indent = true;
+			var mapper = MappingProfile.InitializeAutoMapper().CreateMapper();
+			FluentValidationModelValidatorProvider.Configure(config);
+			var container = new UnityContainer();
+			container.RegisterType<IUnitOfWork, UnitOfWork>(new HierarchicalLifetimeManager());
+			container.RegisterInstance<IMapper>(mapper);
+			config.DependencyResolver = new UnityResolver(container);
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
 }
